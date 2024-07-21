eda8fe643c70bf49c44ce32a760fb0e5c0ed550c
OfficialShubhamRane <official.shubhamrane@gmail.com>
2024-07-21 11:43:00 -0500

    created custom publisher and subscriber

---
+++ udemy_ros2_pkg/scripts/subscriber.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class HelloWorldSubscriber(Node):
+    def __init__(self):
+        super().__init__("Hello_world_sub_node") # create a node (Hello_world_sub_node)
+        self.subscriber_1 = self.create_subscription(String, "hello_world", self.subscriber_callback, 10) # make the node subscribe to topic(hello_world)
+        
+    def subscriber_callback(self, msg):
+        print("Received: " + msg.data)
+
+
+def main(args=None):
+    rclpy.init()
+    my_sub = HelloWorldSubscriber()
+    print("Waiting for data to be published...")
+
+    try:
+        rclpy.spin(my_sub) # keeps the node running
+    except KeyboardInterrupt:
+        print("Terminating node...")
+        my_sub.destroy_node() # destroys node on key board intrruption (ctrl+c)
+
+
+if __name__ == '__main__':
+    main()
